81e3a6f843cc1bfb9f93bf99228f70f7779923ed
SHUNYING <2425957741@qq.com>
2025-03-18 17:30:47 +0800

    bjy

---
+++ chatbot-api-domain/src/main/java/chatbot/api/domain/package-info.java
@@ -0,0 +1,1 @@
+package chatbot.api.domain;
---
+++ chatbot-api-interfaces/src/test/java/chatbot/api/test/ApiTest.java
@@ -0,0 +1,11 @@
+package chatbot.api.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiTest {
+    public static void main(String[] args) {
+        SpringApplication.run(ApiTest.class, args);
+    }
+}
